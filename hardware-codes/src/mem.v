b167131c7e459b4558dc69480e158dd4db69b4d4
leesou <1181902665@qq.com>
2022-12-18 20:40:44 +0800

    modify memory initialization

--- hardware-codes/src/mem.v
+++ hardware-codes/src/mem.v
@@ -8,7 +8,7 @@ module ram (
     input WE
 );
 
-parameter LEN = 1048576;
+parameter LEN = 65536;
 
 reg [31:0] mem_core [0:LEN-1];
 
@@ -18,7 +18,9 @@ initial begin
     for(i=0;i<=LEN-1;i=i+1) begin
         mem_core[i] = 0;
     end
-    $readmemh("tb/test_cpu/tb_ram.hex",mem_core);
+    if (`TEST_TYPE == 0) begin
+        $readmemh("tb/test_cpu/tb_ram.hex",mem_core);
+    end
     for(i=0; i<=13; i=i+1) begin
         $display("%h", mem_core[i]);
     end
@@ -43,7 +45,7 @@ module rom (
     input [31:0] A
 );
 
-parameter LEN = 1048576;
+parameter LEN = 1024;
 
 reg [31:0] mem_core [0:LEN-1];
 
@@ -53,7 +55,14 @@ initial begin
     for(i=0;i<=LEN-1;i=i+1) begin
         mem_core[i] = 0;
     end
-    $readmemh("tb/test_cpu/tb_code.hex",mem_core);
+
+    if (`TEST_TYPE == 0) begin
+        $readmemh("tb/test_cpu/tb_code.hex", mem_core);
+    end
+    if (`TEST_TYPE == 1) begin
+        $readmemh("tb/one_tile_matmul/one_tile_matmul.hex", mem_core);
+    end
+
     for(i=0; i<=13; i=i+1) begin
         $display("%h", mem_core[i]);
     end
