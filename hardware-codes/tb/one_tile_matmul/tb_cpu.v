aaa139ba18ef431de9451fbe7bbf268b67ff6a43
leesou <1181902665@qq.com>
2022-12-28 21:02:57 +0800

    update tb code

--- hardware-codes/tb/one_tile_matmul/tb_cpu.v
+++ hardware-codes/tb/one_tile_matmul/tb_cpu.v
@@ -4,6 +4,9 @@
 `define __RESETPC__ 32'd0
 
 `define TEST_TYPE 1 // 0: test CPU, 1: test one-tile matmul, 2: test full matmul
+`define OUTPUT_ADDR 32768
+`define OUTPUT_HEIGHT 8
+`define OUTPUT_WIDTH 16
 
 `include "src/code.v"
 `include "src/mem.v"
@@ -26,6 +29,8 @@ wire [31:0] IDATA, IADDR, DATAI, DATAO, DADDR;
 initial CLK = 0;
 always #2 CLK = ~CLK;
 
+reg ram_debug;
+
 reg cim_debug;
 reg cs;
 wire write, cim, partial_sum, reset_output;
@@ -36,15 +41,18 @@ wire [31:0] mem_output, cim_output;
 initial begin
     cs = 1;
     cim_debug = 0;
+    ram_debug = 0;
     RES = 1;
     HLT = 0;
     #1
     RES = 0;
 
-    #1000
-    //cim_debug = 1;
-    //#2 
-    //cim_debug = 0;
+    #65536
+    cim_debug = 1;
+    ram_debug = 1;
+    #2 
+    cim_debug = 0;
+    ram_debug = 0;
     $stop;
 end
 
@@ -53,7 +61,8 @@ ram uram (
     .D ( DATAO ) ,
     .Q ( DATAI ) ,
     .A ( DADDR ) ,
-    .WE ( WR )
+    .WE ( WR ),
+    .DEBUG(ram_debug)
 );
 
 rom urom (
