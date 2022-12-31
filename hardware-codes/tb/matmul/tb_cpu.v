317e9c64943471c289b78eaf9722cf43f335d20a
leesou <1181902665@qq.com>
2022-12-31 21:35:07 +0800

    add test bench and test data

--- hardware-codes/tb/matmul/tb_cpu.v
+++ hardware-codes/tb/matmul/tb_cpu.v
@@ -3,6 +3,11 @@
 `define __RESETSP__ 32'd512
 `define __RESETPC__ 32'd0
 
+`define TEST_TYPE 2 // 0: test CPU, 1: test one-tile matmul, 2: test full matmul
+`define OUTPUT_ADDR 43776
+`define OUTPUT_HEIGHT 16
+`define OUTPUT_WIDTH 196
+
 `include "src/code.v"
 `include "src/mem.v"
 `include "src/cim.v"
@@ -24,6 +29,8 @@ wire [31:0] IDATA, IADDR, DATAI, DATAO, DADDR;
 initial CLK = 0;
 always #2 CLK = ~CLK;
 
+reg ram_debug;
+
 reg cim_debug;
 reg cs;
 wire write, cim, partial_sum, reset_output;
@@ -34,15 +41,18 @@ wire [31:0] mem_output, cim_output;
 initial begin
     cs = 1;
     cim_debug = 0;
+    ram_debug = 0;
     RES = 1;
     HLT = 0;
     #1
     RES = 0;
 
-    #1000
-    //cim_debug = 1;
-    //#2 
-    //cim_debug = 0;
+    #1048576
+    cim_debug = 1;
+    ram_debug = 1;
+    #2 
+    cim_debug = 0;
+    ram_debug = 0;
     $stop;
 end
 
@@ -51,7 +61,8 @@ ram uram (
     .D ( DATAO ) ,
     .Q ( DATAI ) ,
     .A ( DADDR ) ,
-    .WE ( WR )
+    .WE ( WR ),
+    .DEBUG(ram_debug)
 );
 
 rom urom (
